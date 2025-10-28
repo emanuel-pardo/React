dcf8e22a12a8da35c7ab623999351bef3cead669
Emanuel Pardo <epardo-ext@aeropuertosargentina.com>
2025-10-27 22:45:28 -0300

    *Correccion contenido items *Preparacion css home

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,16 +1,14 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import './ItemCard.css';
 
-function ItemCard() {
+function ItemCard({product}) {
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+    <Card className="card-container">
+      <Card.Img variant="top" src={product.img} alt={product.code}/>
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
+        <Card.Title>{product.name}</Card.Title>
+        <Card.Text style={{overflow: "hidden", textOverflow: "ellipsis", whiteSpace: "nowrap"}}>{product.description}</Card.Text>       
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ItemCard from '../ItemCard/ItemCard'
+import './ItemList.css';
 
 const ItemList = ({ data }) => {
     return (
-        <div>
-            {data?.map((item) => <ItemCard key={item.id} product={item} />)}
+        <div  className="item-list">
+            {data?.map((item) => <ItemCard key={item.id} product={item}/>)}
         </div>
 
     )
