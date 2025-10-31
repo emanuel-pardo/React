77fd34bbdada9d68b5cade8fd3ba669bb06e32fd
Emanuel Pardo <epardo-ext@aeropuertosargentina.com>
2025-10-30 22:11:21 -0300

    *detalles

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from "react";
 import "./ItemDetail.css";
 
-const ItemDetail = ({product}) => {
- 
+const ItemDetail = ({ product }) => {
     return (
         <>
             <div className='detail-container'>
-                <img src={product?.img}alt={product?.code} className="detail-img"/>
-                <h1 className="detail-title">{product?.name}</h1>
-                <h2 className="detail-subtitle">{product?.code} </h2>
-                <p className="detail-text">{product?.description}</p>
-                <h2 className="detail-subtitle">{product?.category} </h2>
+                <img src={product?.img} alt={product?.code} className="detail-img" />
+                <h1 className="detail-title">{product?.name?.toUpperCase() ?? ""}</h1>
+                <h2 className="detail-title">{product?.category?.toUpperCase() ?? ""} </h2>
+                <h3 className="detail-subtitle">{product?.code ?? ""} </h3>
+                <p className="detail-text">{product?.description ?? ""}</p>
+                <p className="detail-numbers">${product?.price ?? 0}</p>
+                <p className="detail-numbers">{product?.stock ?? 0}</p>
             </div>
         </>
     )
